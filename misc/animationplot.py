54be4408e1f02290534d0784301823d92efced46
Joan Reyero <joanreyero@gmail.com>
2020-11-08 16:09:23 +0000

    not much new

--- misc/animationplot.py
+++ misc/animationplot.py
@@ -5,10 +5,45 @@ from matplotlib.animation import FuncAnimation
 class RangeVisualiser():
 
     def __init__(self):
-        self.fig, self.ax = plt.subplots()
-        self.ln, = plt.matshow([])
+        self.fig = plt.figure()
 
-    def plot_init(self):
-        return self.ln
+        self.init_array = np.zeros((100, 100))
+        self.img = plt.imshow(self.init_array, animated=True)
+
+    def update(self, frames):
+        self.img.set_array(np.random.normal(size=(100, 100)))
+        return self.img
+
+
+if __name__ == '__main__':
+    rv = RangeVisualiser()
+    ani = FuncAnimation(rv.fig, rv.update, interval=50, blit=True)
+    plt.show()
+    # import numpy as np
+    # import matplotlib.pyplot as plt
+    # import matplotlib.animation as animation
+
+    # fig = plt.figure()
+
+
+    # def f(x, y):
+    #     return np.sin(x) + np.cos(y)
+
+    # x = np.linspace(0, 2 * np.pi, 120)
+    # y = np.linspace(0, 2 * np.pi, 100).reshape(-1, 1)
+
+    # im = plt.imshow(f(x, y), animated=True)
+
+
+    # def updatefig(*args):
+    #     global x, y
+    #     x += np.pi / 15.
+    #     y += np.pi / 20.
+    #     im.set_array(f(x, y))
+    #     return im,
+
+    # ani = animation.FuncAnimation(fig, updatefig, interval=50, blit=True)
+    # plt.show(block=True)
+
 
 
